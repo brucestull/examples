b87da03d0134016e0e0383a379bc13d43f90ac0c
Bruce Stull <47562501+brucestull@users.noreply.github.com>
2022-09-01 12:31:30 -0400

    Changed function-based view from 'index' to 'index_view'.

--- django/list_first/the_app/views.py
+++ django/list_first/the_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def index(request):
+def index_view(request):
     the_return_string = 'Goodbuy, World! Enjoy the sail!'
     return HttpResponse(the_return_string)
