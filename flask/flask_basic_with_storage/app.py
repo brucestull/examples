652443ef090852062d45fbea7144a531f7b30a95
Bruce Stull <47562501+brucestull@users.noreply.github.com>
2022-10-14 05:07:35 -0400

    Remove blank lines

--- flask/flask_basic_with_storage/app.py
+++ flask/flask_basic_with_storage/app.py
@@ -8,17 +8,13 @@ STORAGE_INDEX = 0
 @app.route('/', methods=["GET", "POST"])
 def index():
     if request.method == 'POST':
-
         if STORAGE_LIST_OBJECT:
             STORAGE_LIST_OBJECT.pop(STORAGE_INDEX)
-
         the_text_from_the_html_form = request.form['the_text']
-
         STORAGE_LIST_OBJECT.insert(
             STORAGE_INDEX,
             the_text_from_the_html_form
         )
-
         return redirect('/')
 
     return render_template(
