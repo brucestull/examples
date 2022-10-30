82c06c82fefbe636e91c3c2586c8f2a1cd2406ac
Bruce Stull <47562501+brucestull@users.noreply.github.com>
2022-10-30 04:57:09 -0400

    Add print statements

--- django/POST-and-GET/the_app/views.py
+++ django/POST-and-GET/the_app/views.py
@@ -6,8 +6,12 @@ from django.urls import reverse
 def index(request):
     if request.method == 'POST':
         print('request.method: ', request.method)
+        print("Do 'POST' logic here.")
+        # Use `HttpResponseRedirect` so user does not submit same form
+        #     multiple times.
         return HttpResponseRedirect(reverse('the_app_name:index_view_name'))
     # If request.method is not `POST` it will probably be `GET`.
     else:
         print('request.method: ', request.method)
+        print("Do 'GET' logic here.")
         return render(request, 'home.html')
