ab77b037b57975f3abb4b2d2da239d0af1774ee7
Bruce Stull <47562501+brucestull@users.noreply.github.com>
2022-11-07 18:21:19 -0500

    Add stuff

--- javascript/javascriptAttributes/main.js
+++ javascript/javascriptAttributes/main.js
@@ -6,9 +6,12 @@ console.log('numbers: ', numbers)
 let firstUserInput = 'zero'
 
 console.log('firstUserInput: ', firstUserInput)
+// firstUserInput:  zero
 
 console.log('numbers.zero: ', numbers.zero)
+// numbers.zero:  Is the literal 'zero'!
 console.log('numbers.one: ', numbers.one)
+// numbers.one:  Is the literal 'one'?
 
 // Now, try to get the value, which the user enters, from the object 'numbers'.
 
